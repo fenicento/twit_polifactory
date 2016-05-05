cdda22317e513b4f563a68a4db004b1e4a93c529
fenicento <stonecoldcrazy@gmail.com>
2016-05-05 15:48:28 +0200

    wind

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /**
  * Created by Cento on 04/05/2016.
  */
-var Twit = require('twit')
-
+var Twit = require('twit');
+var request = require("request");
 var T = new Twit({
     consumer_key:         'fYC8BHQFLfs8l4d5MHGA',
     consumer_secret:      '65IeofnbybLC4AUlJeKEnhV8eOF6HS8w2aAaGKl9iw',
@@ -61,7 +61,7 @@ nytimes.on('tweet', function (tweet) {
             }
         }
     }
-    console.log(count);
+   // console.log(count);
 
 });
 
@@ -69,10 +69,79 @@ nytimes.on('tweet', function (tweet) {
 var express = require('express');
 var app = express();
 
+
+
+
+
+var url1= "https://api.import.io/store/connector/e39e8693-8dae-402f-b46d-779c8a0164e9/_query?input=webpage/url:http%3A%2F%2Fmagicseaweed.com%2FBells-Beach-Surf-Report%2F524%2F&&_apikey=b107e5e7a10f49b7b7ed7882c07d4cdc5eb962dcfdbef4cebbf574c96d7fe2234330362971bdb13a1e8856d1cce9b3377c1bd2d4b0d78f3bde66bb8dde104f0a71c933d48ba3d0148366a08e28dccdbb";
+var url2= "https://api.import.io/store/connector/d4427531-40ad-4bb9-9d5f-1f23c4657079/_query?input=webpage/url:http%3A%2F%2Fmagicseaweed.com%2FSupertubos-Surf-Report%2F196%2F&&_apikey=b107e5e7a10f49b7b7ed7882c07d4cdc5eb962dcfdbef4cebbf574c96d7fe2234330362971bdb13a1e8856d1cce9b3377c1bd2d4b0d78f3bde66bb8dde104f0a71c933d48ba3d0148366a08e28dccdbb";
+var url3 = "https://api.import.io/store/connector/13a26a89-971c-4925-a7ae-ac04cc9a0663/_query?input=webpage/url:http%3A%2F%2Fmagicseaweed.com%2FPeahi-Jaws-Surf-Report%2F617%2F&&_apikey=b107e5e7a10f49b7b7ed7882c07d4cdc5eb962dcfdbef4cebbf574c96d7fe2234330362971bdb13a1e8856d1cce9b3377c1bd2d4b0d78f3bde66bb8dde104f0a71c933d48ba3d0148366a08e28dccdbb";
+
+
+app.get('/loc1', function(req, res) {
+
+    request(url1, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var obj = JSON.parse(body).results[0];
+
+
+            obj.wind_direction = obj.wind_direction.match(/[0-9]+/)[0];
+            console.log(obj); // Show the HTML for the Google homepage.
+            res.json(obj);
+        }
+    })
+
+
+    //res.type('text/plain');
+    //res.json(count);
+});
+
+
+
+app.get('/loc2', function(req, res) {
+
+    request(url2, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var obj = JSON.parse(body).results[0];
+
+
+            obj.wind_direction = obj.wind_direction.match(/[0-9]+/)[0];
+            console.log(obj); // Show the HTML for the Google homepage.
+            res.json(obj);
+        }
+    })
+
+
+    //res.type('text/plain');
+    //res.json(count);
+});
+
+
+app.get('/loc3', function(req, res) {
+
+    request(url3, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var obj = JSON.parse(body).results[0];
+
+
+            obj.wind_direction = obj.wind_direction.match(/[0-9]+/)[0];
+            console.log(obj); // Show the HTML for the Google homepage.
+            res.json(obj);
+        }
+    })
+
+
+    //res.type('text/plain');
+    //res.json(count);
+});
+
 app.get('/', function(req, res) {
     //res.type('text/plain');
     res.json(count);
 });
+
+
+
 
 
 app.listen(process.env.PORT || 8080);
@@ -97,3 +166,6 @@ club.on('tweet', function (tweet) {
 //byblos.on('tweet', function (tweet) {
 //    console.log(tweet.text)
 //});
+
+
+
