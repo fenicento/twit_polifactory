2b349a871a010c9c039a41216bb3e03741ef6510
fenicento <stonecoldcrazy@gmail.com>
2016-05-05 17:58:05 +0200

    patch for wind

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
  */
 var Twit = require('twit');
 var request = require("request");
+var csv = require('express-csv')
+
+
 var T = new Twit({
     consumer_key:         'fYC8BHQFLfs8l4d5MHGA',
     consumer_secret:      '65IeofnbybLC4AUlJeKEnhV8eOF6HS8w2aAaGKl9iw',
@@ -62,16 +65,11 @@ nytimes.on('tweet', function (tweet) {
         }
     }
    // console.log(count);
-
 });
 
 
 var express = require('express');
 var app = express();
-
-
-
-
 
 var url1= "https://api.import.io/store/connector/e39e8693-8dae-402f-b46d-779c8a0164e9/_query?input=webpage/url:http%3A%2F%2Fmagicseaweed.com%2FBells-Beach-Surf-Report%2F524%2F&&_apikey=b107e5e7a10f49b7b7ed7882c07d4cdc5eb962dcfdbef4cebbf574c96d7fe2234330362971bdb13a1e8856d1cce9b3377c1bd2d4b0d78f3bde66bb8dde104f0a71c933d48ba3d0148366a08e28dccdbb";
 var url2= "https://api.import.io/store/connector/d4427531-40ad-4bb9-9d5f-1f23c4657079/_query?input=webpage/url:http%3A%2F%2Fmagicseaweed.com%2FSupertubos-Surf-Report%2F196%2F&&_apikey=b107e5e7a10f49b7b7ed7882c07d4cdc5eb962dcfdbef4cebbf574c96d7fe2234330362971bdb13a1e8856d1cce9b3377c1bd2d4b0d78f3bde66bb8dde104f0a71c933d48ba3d0148366a08e28dccdbb";
@@ -83,11 +81,13 @@ app.get('/loc1', function(req, res) {
     request(url1, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var obj = JSON.parse(body).results[0];
-
-
-            obj.wind_direction = obj.wind_direction.match(/[0-9]+/)[0];
-            console.log(obj); // Show the HTML for the Google homepage.
-            res.json(obj);
+            var fin = {}
+            fin.swell_period = parseFloat(obj.swell_period)
+            fin.swell_height = parseFloat(obj.swell_height)
+            fin.wind_speed = parseFloat(obj.wind_speed)
+            fin.wind_direction = parseFloat(obj.wind_direction.match(/[0-9]+/)[0]);
+            console.log(fin); // Show the HTML for the Google homepage.
+            res.json(fin);
         }
     })
 
@@ -96,8 +96,6 @@ app.get('/loc1', function(req, res) {
     //res.json(count);
 });
 
-
-
 app.get('/loc2', function(req, res) {
 
     request(url2, function (error, response, body) {
@@ -105,7 +103,7 @@ app.get('/loc2', function(req, res) {
             var obj = JSON.parse(body).results[0];
 
 
-            obj.wind_direction = obj.wind_direction.match(/[0-9]+/)[0];
+            obj.wind_direction = parseFloat(obj.wind_direction.match(/[0-9]+/)[0]);
             console.log(obj); // Show the HTML for the Google homepage.
             res.json(obj);
         }
@@ -124,7 +122,7 @@ app.get('/loc3', function(req, res) {
             var obj = JSON.parse(body).results[0];
 
 
-            obj.wind_direction = obj.wind_direction.match(/[0-9]+/)[0];
+            obj.wind_direction = parseFloat(obj.wind_direction.match(/[0-9]+/)[0]);
             console.log(obj); // Show the HTML for the Google homepage.
             res.json(obj);
         }
@@ -135,14 +133,57 @@ app.get('/loc3', function(req, res) {
     //res.json(count);
 });
 
-app.get('/', function(req, res) {
+
+
+app.get('/loc1csv', function(req, res) {
+
+    request(url1, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var obj = JSON.parse(body).results[0];
+
+
+            obj.wind_direction = parseFloat(obj.wind_direction.match(/[0-9]+/)[0]);
+            console.log(obj); // Show the HTML for the Google homepage.
+            res.csv([obj]);
+        }
+    })
+})
+    app.get('/loc2csv', function(req, res) {
+
+        request(url2, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var obj = JSON.parse(body).results[0];
+
+
+                obj.wind_direction = parseFloat(obj.wind_direction.match(/[0-9]+/)[0]);
+                console.log(obj); // Show the HTML for the Google homepage.
+                res.csv([obj]);
+            }
+        })
+    })
+        app.get('/loc3csv', function(req, res) {
+
+            request(url3, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var obj = JSON.parse(body).results[0];
+
+
+                    obj.wind_direction = obj.wind_direction.match(/[0-9]+/)[0];
+                    console.log(obj); // Show the HTML for the Google homepage.
+                    res.csv([obj]);
+                }
+            })
+
     //res.type('text/plain');
-    res.json(count);
+    //res.json(count);
 });
 
 
 
-
+app.get('/', function(req, res) {
+    //res.type('text/plain');
+    res.json(count);
+});
 
 app.listen(process.env.PORT || 8080);
 /*
